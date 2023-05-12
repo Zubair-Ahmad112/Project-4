601496cb2c2169bc6608d417ab539b8f2c19ef25
Zubair-Ahmad112 <zubair.ahmed@tutanota.com>
2023-05-12 20:53:58 +0500

    Modifications

--- ehs/settings.py
+++ ehs/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&@-o*pb+_$x+gclovr4v98-(+-*@ey0yi+7qm(2g8i5#p6zt@8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['ehs.herokuapp.com']
